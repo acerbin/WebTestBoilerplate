f268fb0d50318b522a8b7e7533f53f4a19b17dbb
Katrina Savova <katrina@gamingrealms.com>
2021-05-17 17:37:26 +0100

    Add server request validation

--- cypress/integration/basic/test_spec.js
+++ cypress/integration/basic/test_spec.js
@@ -1,12 +1,24 @@
-describe('Launch a platform', () => {
+describe('Launch google and perform a search', () => {
 
     beforeEach(() => {
         cy.visit('https://google.com')
+        cy.contains('I agree').click()
       })
 
     it('Validates the page is loaded', () => {
-        cy.contains('I agree').click()
-        cy.get('.gLFyf').type("Cypress")
-        cy.takeSnapshot('google', {x: 300, y: 0, width: 420, height:490});
+      cy.url().should('include', 'google') 
+      cy.takeSnapshot('google', {x: 300, y: 0, width: 420, height:490});
+    })
+
+    it('Verifies server side request', () => {
+      cy.server();
+      cy.route('GET','/complete/search?**').as('search');
+      cy.get('.gLFyf').type("Cypress")
+      cy.get('.lnXdpd').click()
+      cy.get('.FPdoLc > center > .gNO89b').click()
+      cy.wait('@search').its('status').should('eq',null); //200, 201, etc.
+      // cy.get('@search').should((xhr) => {
+      //   expect(xhr.responseBody).to.have.property("a","b"); //verify server response
+      // });
     })
   })
